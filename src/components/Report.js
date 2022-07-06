3a50b20d72bae6622ea5956712ffee2af2d8d0c2
jinwookShin <0201jinwook_@gmail.com>
2022-07-06 16:00:22 +0900

    feat: styled components 추가 및  사용자 profile과 contents 뿌려주는 항목 추가

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,13 +1,38 @@
 import styled from 'styled-components';
 
-const Image = styled.image``;
+const Image = styled.img``;
+const Profile = styled.div``;
+const Contents = styled.div``;
 
-function Report({instagramUser}) {
+function Report({ instagramUser }) {
+  const profile = instagramUser.data.profile;
+  const contents = instagramUser.data.contents;
+
   return (
     <>
-      <h2>{instagramUser.data.profile.name}</h2>
-      <h2>{instagramUser.data.profile.username}</h2>
-      <Image src={instagramUser.data.profile.profileImgSrc} alt={`${instagramUser.data.profile.profileImgAlt} thumbnail`} />
+      <Profile>
+        <h2>{profile.name}</h2>
+        <h2>{profile.username}</h2>
+        <Image src={profile.profileImgSrc} alt={`${profile.profileImgAlt} thumbnail`} />
+        <p>{profile.introduction}</p>
+        <div><span>numberOfPosts</span>{profile.numberOfPosts}</div>
+        <div><span>numberOfFollowers</span>{profile.numberOfFollowers}</div>
+        <div><span>numberOfFollowings</span>{profile.numberOfFollowings}</div>
+      </Profile>
+      <Contents>
+        <ul>
+          {contents.posts.map((post, index) => {
+            return <li key={index}>
+              <div>{post.location}</div>
+              <img src={post.imgSrc} alt={post.description} />
+              <div>{post.numberOfLikes}</div>
+              <div>{post.numberOfReplies}</div>
+              <p>{post.description}</p>
+              <div>{post.datePosted}</div>
+            </li>
+          })}
+        </ul>
+      </Contents>
     </>
   );
 }
