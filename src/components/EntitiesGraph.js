6d5bf85e0b8e58876a6f063a8f1cd2dea8880142
jinwookShin <0201jinwook_@gmail.com>
2022-07-13 17:39:18 +0900

    feat: posts 없을 시 옵셔널 체이닝으로 그래프 미표시 기능 추가

--- src/components/EntitiesGraph.js
+++ src/components/EntitiesGraph.js
@@ -1,87 +1,96 @@
 import * as d3 from "d3";
-import { useRef, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
+import useDeepCompareEffect from 'use-deep-compare-effect';
 
 function EntitiesGraph({ posts }) {
-  const getEntitiesCounter = () => {
-    let entitiesCounter = {};
-
-    if (posts?.length) {
-      posts.forEach((post) => {
-        const entities = post.entitiesResult?.entities;
-        
-        entities.forEach((entity) => {
-          console.log("entity", entity);
-
-          if (entity.metadata && entity.metadata["wikipedia_url"]) {
-            const coreEntity = entity;
-            const entityInfoObj = { count: 1 };
-  
-            if (entitiesCounter[coreEntity.name]) {
-              entitiesCounter[coreEntity.name].count++;
-            } else {
-              Object.assign(entityInfoObj, { wikipedia_url: coreEntity.metadata["wikipedia_url"] });
-              entitiesCounter[coreEntity.name] = entityInfoObj;
-            };
-          }
-        });
-      });
-    }
-
-    return entitiesCounter;
-  }
-
-  const entitiesCounter = getEntitiesCounter();
-  const svgRef = useRef();
+  const [entitiesCounter, setEntitiesCounter] = useState({});
 
   useEffect(() => {
-    const WIDTH = 600;
-    const HEIGHT = 600;
-    const RADIUS = WIDTH / 2;
-    const formattedData = [];
+    const getEntitiesCounter = () => {
+      let entitiesCounter = {};
 
-    for (const [key, value] of Object.entries(entitiesCounter)) {
-      formattedData.push({name: key, count: value.count, wikipedia_url: value.wikipedia_url});
+      if (posts?.length) {
+        posts.forEach((post) => {
+          const entities = post.entitiesResult?.entities;
+
+          entities.forEach((entity) => {
+            if (entity.metadata && entity.metadata["wikipedia_url"]) {
+              const coreEntity = entity;
+              const entityInfoObj = { count: 1 };
+
+              if (entitiesCounter[coreEntity.name]) {
+                entitiesCounter[coreEntity.name].count++;
+              } else {
+                Object.assign(entityInfoObj, { wikipedia_url: coreEntity.metadata["wikipedia_url"] });
+                entitiesCounter[coreEntity.name] = entityInfoObj;
+              };
+            }
+          });
+        });
+      }
+
+      return entitiesCounter;
     }
 
-    const pieData = d3.pie().value(d => d.count)(formattedData);
-    const arcGenerator = d3.arc().innerRadius(0).outerRadius(RADIUS);
-    const color = d3.scaleOrdinal().range(d3.schemeSet3);
+    const entitiesCounter = getEntitiesCounter();
+    setEntitiesCounter(entitiesCounter);
+  }, [posts]);
 
-    const svg = d3.select(svgRef.current)
-      .attr('width', WIDTH)
-      .attr('height', HEIGHT)
-      .append('g')
-      .attr('transform', 'translate(300, 300)');
+  const svgRef = useRef();
 
-    const tooldiv = d3.select('#chartArea')
-      .append('div')
-      .style('visibility', 'hidden')
-      .style('position', 'absolute')
-      .style('background-color', 'black')
-      .style('color', 'white');
+  useDeepCompareEffect(() => {
+    if (Object.keys(entitiesCounter).length) {
+      const WIDTH = 600;
+      const HEIGHT = 600;
+      const RADIUS = WIDTH / 2;
+      const formattedData = [];
 
-    svg.append('g').selectAll('path')
-      .data(pieData)
-      .join('path')
-      .attr('d', arcGenerator)
-      .attr('fill', (d, i) => color(i))
-      .attr('stroke', 'white')
-      .on('mouseover', (event, d) => {
-        tooldiv.style('visibility', 'visible')
-          .text(`${d.data.name}: ${d.data.count}`)
-      })
-      .on('mousemove', (event, d) => {
-        tooldiv.style('top', (event.pageY - 50) + 'px')
-          .style('left', (event.pageX - 50) + 'px')
-      })
-      .on('mouseout', () => {
-        tooldiv.style('visibility', 'hidden')
-      });
+      for (const [key, value] of Object.entries(entitiesCounter)) {
+        formattedData.push({ name: key, count: value.count, wikipedia_url: value.wikipedia_url });
+      }
+
+      const pieData = d3.pie().value(d => d.count)(formattedData);
+      const arcGenerator = d3.arc().innerRadius(0).outerRadius(RADIUS);
+      const color = d3.scaleOrdinal().range(d3.schemeSet3);
+
+      const svg = d3.select(svgRef.current)
+        .attr('width', WIDTH)
+        .attr('height', HEIGHT)
+        .append('g')
+        .attr('transform', 'translate(300, 300)');
+
+      const tooldiv = d3.select('#chartArea')
+        .append('div')
+        .style('visibility', 'hidden')
+        .style('position', 'absolute')
+        .style('background-color', 'black')
+        .style('color', 'white');
+
+      svg.append('g').selectAll('path')
+        .data(pieData)
+        .join('path')
+        .attr('d', arcGenerator)
+        .attr('fill', (d, i) => color(i))
+        .attr('stroke', 'white')
+        .on('mouseover', (event, d) => {
+          tooldiv.style('visibility', 'visible')
+            .text(`${d.data.name}: ${d.data.count}`)
+        })
+        .on('mousemove', (event, d) => {
+          tooldiv.style('top', (event.pageY - 50) + 'px')
+            .style('left', (event.pageX - 50) + 'px')
+        })
+        .on('mouseout', () => {
+          tooldiv.style('visibility', 'hidden')
+        });
+    }
   }, [entitiesCounter]);
 
   return (
     <div id='chartArea'>
-      <svg ref={svgRef} />
+      {
+        Object.keys(entitiesCounter).length && <svg ref={svgRef} />
+      }
     </div>
   );
 }
