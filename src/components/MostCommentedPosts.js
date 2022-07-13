e51b6bb342f36bc8931371fb1cc78f5668a36cba
jinwookShin <0201jinwook_@gmail.com>
2022-07-13 15:56:50 +0900

    feat: MostCommentedPosts, MostLikedPosts 컴포넌트 제작

--- src/components/MostCommentedPosts.js
+++ src/components/MostCommentedPosts.js
@@ -1,37 +1,54 @@
 import styled from 'styled-components';
 import Post from "./Post";
+import { useState, useEffect } from "react";
 
-function MostCommentedPosts({ contents }) {
-  const sortedContents = contents.posts.sort((a, b) => b.numberOfReplies - a.numberOfReplies);
-  const ITERATION = 8;
+
+function MostCommentedPosts({ posts }) {
+  const [sortedPosts, setSortedPosts] = useState(null);
+
+  // const sortedContents = sortedPosts.posts.sort((a, b) => b.numberOfReplies - a.numberOfReplies);
+  const NUMBER_OF_POSTS_TO_SHOW = 5;
   const printSortedContents = () => {
     const array = [];
 
-    for (let i = 0; i < ITERATION; i++) {
-      const post = sortedContents[i];
-
-      array.push(
-        <Post
-          key={i}
-          location={post.location}
-          imgSrc={post.imgSrc}
-          description={post.description}
-          numberOfLikes={post.numberOfLikes}
-          numberOfReplies={post.numberOfReplies}
-          datePosted={post.datePosted}
-        />
-      );
+    if (sortedPosts?.length) { 
+      for (let i = 0; i < NUMBER_OF_POSTS_TO_SHOW; i++) {
+        const post = sortedPosts[i];
+        
+        array.push(
+          <Post
+            key={i}
+            location={post.location}
+            imgSrc={post.imgSrc}
+            description={post.description}
+            numberOfLikes={post.numberOfLikes}
+            numberOfReplies={post.numberOfReplies}
+            datePosted={post.datePosted}
+          />
+        );
+      }
+    } else {
+      array.push(<div>no posts to show</div>);
     }
 
     return array;
   }
 
+  useEffect(() => {
+    const sortedContents = posts?.sort((a, b) => b.numberOfReplies - a.numberOfReplies);
+
+    setSortedPosts(sortedContents);
+  }, [posts]);
+
   return (
-    <ul>
-      {
-        printSortedContents()
-      }
-    </ul>
+    <>
+      <h1>Most Commented</h1>
+      <ul>
+        {
+          printSortedContents()
+        }
+      </ul>
+    </>
   );
 }
 
--- src/components/MostLikedPosts.js
+++ src/components/MostLikedPosts.js
@@ -1,52 +1,79 @@
 import Post from "./Post";
+import { useState, useEffect } from "react";
 
-function MostLikedPosts({ contents }) {
+function MostLikedPosts({ posts }) {
+  const [sortedPosts, setSortedPosts] = useState(null);
   const NUMBER_OF_POSTS_TO_SHOW = 5;
-  const convertSItoNumber = (item) => {
-    if (item[item.length - 1] === "k") {
-      item = Number(item.slice(0, item.length - 1)) * 1000;
+  const convertSItoNumber = (likes) => {
+    let numberOfLikes = likes;
+    
+    if (numberOfLikes[numberOfLikes.length - 1] === "k") {
+      numberOfLikes = Number(numberOfLikes.slice(0, numberOfLikes.length - 1)) * 1000;
+    } else if (numberOfLikes[numberOfLikes.length - 1] === "m") {
+      numberOfLikes = Number(numberOfLikes.slice(0, numberOfLikes.length - 1)) * 1000000;
+    } else {
+      Number(numberOfLikes);
     }
 
-    if (item[item.length - 1] === "m") {
-      item = Number(item.slice(0, item.length - 1)) * 1000000;
-    }
-
-    return item;
+    return numberOfLikes;
   };
-  const sortedContents = contents.posts.sort((a, b) => {
-    const formerNumberOfLikes = convertSItoNumber(a.numberOfLikes);
-    const latterNumberOfLikes = convertSItoNumber(b.numberOfLikes);
-
-    return latterNumberOfLikes - formerNumberOfLikes;
-  });
-  const printSortedContents = () => {
+  const printSortedContents = (sortedPosts) => {
     const array = [];
 
-    for (let i = 0; i < NUMBER_OF_POSTS_TO_SHOW; i++) {
-      const post = sortedContents[i];
-
-      array.push(
-        <Post
-          key={i}
-          location={post.location}
-          imgSrc={post.imgSrc}
-          description={post.description}
-          numberOfLikes={post.numberOfLikes}
-          numberOfReplies={post.numberOfReplies}
-          datePosted={post.datePosted}
-        />
-      );
+    if (sortedPosts?.length) {
+      for (let i = 0; i < NUMBER_OF_POSTS_TO_SHOW; i++) {
+        const post = sortedPosts[i];
+  
+        array.push(
+          <Post
+            key={post.id}
+            location={post.location}
+            imgSrc={post.imgSrc}
+            description={post.description}
+            numberOfLikes={post.numberOfLikes}
+            numberOfReplies={post.numberOfReplies}
+            datePosted={post.datePosted}
+          />
+        );
+      }
+    } else {
+      array.push(<div>no posts to show</div>);
     }
 
     return array;
   };
 
+  useEffect(() => {
+    console.log("posts", posts);
+    
+
+    const sortedContents = posts?.sort((a, b) => {
+      const formerNumberOfLikes = convertSItoNumber(a.numberOfLikes);
+      const latterNumberOfLikes = convertSItoNumber(b.numberOfLikes);
+
+      // console.log("formerNumberOfLikes", formerNumberOfLikes);
+      // console.log("latterNumberOfLikes", latterNumberOfLikes);
+      
+      
+      // return formerNumberOfLikes - latterNumberOfLikes;
+      return latterNumberOfLikes - formerNumberOfLikes;
+    });
+
+    console.log("sortedContents", sortedContents);
+    
+    // printSortedContents(sortedContents);
+    setSortedPosts(sortedContents);
+  }, [posts]);
+
   return (
-    <ul>
-      {
-        printSortedContents()
-      }
-    </ul>
+    <>
+      <h1>Most Liked</h1>
+      <ul>
+        {
+          printSortedContents(sortedPosts)
+        }
+      </ul>
+    </>
   );
 }
 
