4e843432b5f02ba361f332d5276cda7bca7ed96b
jinwookShin <0201jinwook_@gmail.com>
2022-07-17 21:12:45 +0900

    refactor: 줄간격 및 기타 리팩토링

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import Canvas from './Canvas';
 
 const List = styled.li``;
 const Location = styled.div``;
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+  object-fit: contain;
+`;
 const NumberOfLikes = styled.div``;
 const NumberOfReplies = styled.div``;
 const Description = styled.p``;
 const DatePosted = styled.div``;
 
-function Post({ location, imgSrc, description, numberOfLikes, numberOfReplies, datePosted }) {
+function Post({ location, imgSrc, description, numberOfLikes, numberOfReplies, datePosted, prediction }) {
+  const [isClicked, setIsClicked] = useState(false);
+  const onImageClick = () => {
+    isClicked ? setIsClicked(false) : setIsClicked(true);
+  }
+  
   return (
     <List>
       <Location>{location}</Location>
-      <Image src={imgSrc} alt={description} />
-      <NumberOfLikes>{numberOfLikes}</NumberOfLikes>
-      <NumberOfReplies>{numberOfReplies}</NumberOfReplies>
+      { !isClicked && <Image src={imgSrc} alt={description} onClick={onImageClick} /> }
+      { isClicked && <Canvas src={imgSrc} prediction={prediction} setIsClicked={setIsClicked} /> }
+      <NumberOfLikes>num of likes {numberOfLikes}</NumberOfLikes>
+      <NumberOfReplies>num of replies{numberOfReplies}</NumberOfReplies>
       <Description>{description}</Description>
       <DatePosted>{datePosted}</DatePosted>
     </List>
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,38 +1,73 @@
 import styled from 'styled-components';
+import Post from "./Post";
+import LocationsGraph from "./LocationsGraph";
+import MostLikedPosts from './MostLikedPosts';
+import MostCommentedPosts from './MostCommentedPosts';
+import CategoriesGraph from "./CategoriesGraph";
+import SentimentsGraph from "./SentimentsGraph";
+import EntitiesGraph from "./EntitiesGraph";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Image = styled.img``;
 const Profile = styled.div``;
 const Contents = styled.div``;
+const Name = styled.h2``;
+const Username = styled.h2``;
+const Introduction = styled.p``;
+const NumberOfPosts = styled.div``;
+const NumberOfFollowers = styled.div``;
+const NumberOfFollowings = styled.div``;
 
-function Report({ instagramUser }) {
-  const profile = instagramUser.data.profile;
-  const contents = instagramUser.data.contents;
+function Report({ setIsLoading, isLoading, response }) {
+  let { username, reportId } = useParams();
+  const [dbUser, setDBuser] = useState(null); 
+
+  useEffect(() => {
+    async function fetchReport() {
+      try {
+        const response = await axios({
+          method: "get",
+          url: `http://IGUR-backend-dev.ap-northeast-2.elasticbeanstalk.com/users/${username}/reports/${reportId}`,
+        });
+
+        setDBuser(response.data);
+      } catch (err) {
+        console.log("err in report: ", err);
+      }
+    }
+    if (!dbUser) {
+      fetchReport();
+    }
+  }, [dbUser, reportId, username]);
 
   return (
     <>
-      <Profile>
-        <h2>{profile.name}</h2>
-        <h2>{profile.username}</h2>
-        <Image src={profile.profileImgSrc} alt={`${profile.profileImgAlt} thumbnail`} />
-        <p>{profile.introduction}</p>
-        <div><span>numberOfPosts</span>{profile.numberOfPosts}</div>
-        <div><span>numberOfFollowers</span>{profile.numberOfFollowers}</div>
-        <div><span>numberOfFollowings</span>{profile.numberOfFollowings}</div>
-      </Profile>
-      <Contents>
-        <ul>
-          {contents.posts.map((post, index) => {
-            return <li key={index}>
-              <div>{post.location}</div>
-              <img src={post.imgSrc} alt={post.description} />
-              <div>{post.numberOfLikes}</div>
-              <div>{post.numberOfReplies}</div>
-              <p>{post.description}</p>
-              <div>{post.datePosted}</div>
-            </li>
-          })}
-        </ul>
-      </Contents>
+      <h1>Report</h1>
+      { dbUser ?
+        <div>
+          <Profile>
+            <Name>{dbUser?.reports[0].profile.name}</Name>
+            <Username>{dbUser?.reports[0].profile.username}</Username>
+            <Image src={dbUser?.reports[0].profile.profileImgSrc} alt={`${dbUser?.reports[0].profile.profileImgAlt} thumbnail`} />
+            <Introduction>{dbUser?.reports[0].profile.introduction}</Introduction>
+            <NumberOfPosts><span>numberOfPosts</span>{dbUser?.reports[0].profile.numberOfPosts}</NumberOfPosts>
+            <NumberOfFollowers><span>numberOfFollowers</span>{dbUser?.reports[0].profile.numberOfFollowers}</NumberOfFollowers>
+            <NumberOfFollowings><span>numberOfFollowings</span>{dbUser?.reports[0].profile.numberOfFollowings}</NumberOfFollowings>
+          </Profile>
+          <hr></hr>
+          <Contents>
+            <MostLikedPosts posts={dbUser?.reports[0].contents.posts} />
+            <MostCommentedPosts posts={dbUser?.reports[0].contents.posts} />
+            <CategoriesGraph posts={dbUser?.reports[0].contents.posts} />
+            <EntitiesGraph posts={dbUser?.reports[0].contents.posts} />
+            {/* <SentimentsGraph posts={dbUser?.reports[0].contents.posts} /> */}
+            {/* <LocationsGraph posts={dbUser?.reports[0].contents}/> */}
+          </Contents>
+        </div>
+        : "report doesn't exist"
+      }
     </>
   );
 }
