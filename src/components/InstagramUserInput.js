487039fccfb7e5be0b1e4dd3a2b50ccb82238cf0
jinwookShin <0201jinwook_@gmail.com>
2022-07-17 21:04:54 +0900

    feat: 검색창 컴포넌트 추가

--- src/components/InstagramUserInput.js
+++ src/components/InstagramUserInput.js
@@ -1,27 +1,61 @@
 import styled from 'styled-components';
-import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Form = styled.form``;
-const Label = styled.label``;
+const Form = styled.form`
+  padding: 16px 30px;
+  border: 1px solid #dbdbdb;
+  border-radius: 3px;
+  background: #fff;
+`;
+const Settings = styled.ul``;
+const List = styled.li``;
+const Slider = styled.input``;
+const InputContainer = styled.div``;
 const Input = styled.input`
   font-size: 12px;
 `;
 const Button = styled.button``;
 
-function InstagramUserInput({ setInstagramUser }) {
-  const [username, setUsername] = useState();
+function InstagramUserInput({ setResponse, setIsLoading, numberOfCrawls, setNumberOfCrawls, username, setUsername, setIsPrivateAccount }) {
+  const navigate = useNavigate();
   const onInputChange = (e) => {
-    setUsername(e.target.value);
+    e.target.id === "username" && setUsername(e.target.value);
+    e.target.id === "slider" && setNumberOfCrawls(Number(e.target.value));
   };
 
   const onFormSubmit = async (e) => {
+    const reportId = Date.now().toString();
+
     e.preventDefault();
+    setIsLoading(true);
 
     try {
-      const instagramUser = await axios.get(`http://localhost:8000/users/${username}`);
-      
-      setInstagramUser(instagramUser);
+      const response = await axios({
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: "post",
+        // url: `http://localhost:8000/users/${username}`,
+        url: `http://IGUR-backend-dev.ap-northeast-2.elasticbeanstalk.com/users/${username}`,
+        data: { numberOfCrawls, reportId },
+      });
+
+      if (response.status === 204) {
+        setIsLoading(false);
+        navigate(`/users/${username}/reports/${reportId}`);
+      }
+
+      if (response.status === 200) {
+        setIsLoading(false);
+        navigate(`/users/${username}/reports`);
+      }
+
+      if (response.status === 214) {
+        setIsLoading(false);
+        console.log("Private account");
+        setIsPrivateAccount(true);
+      }
     } catch (err) {
       console.error("err: ", err);
     }
@@ -29,11 +63,15 @@ function InstagramUserInput({ setInstagramUser }) {
 
   return (
     <Form onSubmit={onFormSubmit}>
-      <Label>
-        Instagram Id
-        <Input type="input" onChange={onInputChange} />
-      </Label>
-      <Button type="submit">Search user</Button>
+      <InputContainer className="input01">
+        <Input type="text" placeholder='instagram username' id="username" onChange={onInputChange} required />
+      </InputContainer>
+      <Settings>
+        <List>
+          <Slider type="range" step="10" min="0" max="50" defaultValue={numberOfCrawls} id="slider" onChange={onInputChange} />
+        </List>
+      </Settings>
+      <Button type="submit" className="button01">Search</Button>
     </Form>
   );
 }
